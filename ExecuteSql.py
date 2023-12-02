03a70e93882f48cd91002dd1b06a7de79c404315
Leone67 <Leone67>
2023-12-02 00:22:03 +0000

    Added support for semicolon split and execute from file

--- ExecuteSql.py
+++ ExecuteSql.py
@@ -1,17 +1,6 @@
 # Databricks notebook source
-# %pip install databricks-sql-connector
-
-# COMMAND ----------
-
-# MAGIC %pip install --no-index -f ./sql-connector databricks-sql-connector
-
-# COMMAND ----------
-
-dbutils.library.restartPython()
-
-# COMMAND ----------
-
 dbutils.widgets.text("query_sql", "", "SQL to execute")
+dbutils.widgets.text("query_sql_file", "", "SQL to execute from a file")
 dbutils.widgets.text("warehouse_id", "", "SQL Warehouse Id")
 dbutils.widgets.text("host_uri", "adb-984752964297111.11.azuredatabricks.net", "Workspace Host Uri")
 dbutils.widgets.text("max_download_threads", "20", "Maximum threads to use for download if use_cloud_fetch is true")
@@ -19,15 +8,38 @@ dbutils.widgets.text("use_cloud_fetch", "False", "Use Cloud Fetch to retrieve qu
 
 # COMMAND ----------
 
+import re
+
+# COMMAND ----------
+
 host_uri = dbutils.widgets.get("host_uri")
 warehouse_id = dbutils.widgets.get("warehouse_id")
 query_sql = dbutils.widgets.get("query_sql")
+query_sql_file = dbutils.widgets.get("query_sql_file")
 max_download_threads = dbutils.widgets.get("max_download_threads")
 if max_download_threads is not None and max_download_threads != "":
   max_download_threads = int(max_download_threads)
+else:
+  max_download_threads = 20
 use_cloud_fetch = dbutils.widgets.get("use_cloud_fetch")
 
 use_cloud_fetch = use_cloud_fetch.lower() in ['true', '1', 't', 'y', 'yes']
+
+# COMMAND ----------
+
+if query_sql_file is not None and query_sql_file != "":
+    query_sql_from_file = dbutils.fs.head(query_sql_file)
+else:
+    query_sql_from_file = ""
+
+# COMMAND ----------
+
+sql_arr = [s for s in [query_sql, query_sql_from_file] if s is not None and s != ""]
+query_sql = ";".join(sql_arr)
+
+# COMMAND ----------
+
+query_sqls = re.findall("((?:(?:'[^']*')|[^;])*);", query_sql) # split by semicolon
 
 # COMMAND ----------
 
@@ -41,6 +53,7 @@ with sql.connect(server_hostname = host_uri,
                  use_cloud_fetch = use_cloud_fetch) as connection:
 
   with connection.cursor() as cursor:
-    cursor.execute(query_sql)
+    for s in query_sqls:
+      cursor.execute(s)
     results = cursor.fetchall_arrow()
     print(results)
